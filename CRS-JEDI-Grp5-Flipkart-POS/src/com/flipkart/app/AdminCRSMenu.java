13e801f8261bdcb1c8ffb71fb0aef6e16ed3efd6
Harsh Sharma <harsh.sharma8@BLRETV-YDP66WNX.local>
2022-07-28 17:48:25 +0530

    Added Run component

--- CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/app/AdminCRSMenu.java
+++ CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/app/AdminCRSMenu.java
@@ -1,7 +1,28 @@
 package com.flipkart.app;
 
-import java.util.Scanner;
+import java.util.*;
 
-import com.flipkart.bean.Course;
-import com.flipkart.service.AdminServiceImpl;
+public class AdminCRSMenu{
+    private Scanner scanner = new Scanner(System.in);
+    AdminCRSMenu adminCRSMenu = new AdminCRSMenu();
 
+    public void createAdminMenu(String adminId){
+        System.out.println("*************Admin Menu**************");
+        System.out.println("--------------------------------------");
+        System.out.println("Kindly select one option from the menu");
+        System.out.println("--------------------------------------");
+        System.out.println("1. Edit Course Details in the course catalog");
+        System.out.println("2. Add/Remove Professor");
+        System.out.println("3. Add/Remove Admin");
+        System.out.println("4. Enable/Disable Payment window");
+        System.out.println("5. View available Courses for seat availability");
+        System.out.println("6. Approve Student registration");
+        System.out.println("7. View course wise student list");
+        System.out.println("8. View Course wise grades");
+        System.out.println("9. Generate Grade Card");
+        System.out.println("10. Logout");
+        int menuChoice = scanner.nextInt();
+        scanner.nextLine();
+
+    }
+}
--- CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/app/CRSApplication.java
+++ CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/app/CRSApplication.java
@@ -7,98 +7,109 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.HashMap;
-
-import com.flipkart.app.AdminCRSMenu;
 public class CRSApplication {
 	static boolean loggedin = false;
 	Course course = new Course();
 	Map<Integer, course> map = new HashMap<String, course>();
+    Scanner scanner = new Scanner(System.in);
 	
-	public void main(String[]args) {
-		Scanner scanner = new Scanner(System.in);
-		CRSApplication crsApplication=new CRSApplication();
+	public static void main(String[]args) {
 
-		createLoginPage();
-		int usercase;
-		usercase= scanner.nextInt();
-		while(true)
-		{
-			if(usercase == 1){
-				crsApplication.loginUser();
-			}
-			else if(usercase ==2){
-				crsApplication.studentRegistration();
-			}
-			else if(usercase == 3){
-				crsApplication.updatePassword();
-			}
-			else{
-				break;
-			}
-		}
+		CRSApplication crsApplication=new CRSApplication();
+		crsApplication.createLoginPage();
 	}
 
 	public static void createLoginPage()
 	{
-		System.out.println("\n\n----------Course Registration System---------\n\n");
-		System.out.println("1. Login");
-		System.out.println("2. Student Registration");
-		System.out.println("3. Update password");
-		System.out.println("4. Exit");
-		System.out.println("Enter user input");
+        try
+        {
+            while(true)
+            {
+                System.out.println("\n\n----------Course Registration System---------\n\n");
+                System.out.println("1. Login");
+                System.out.println("2. Student Registration");
+                System.out.println("3. Update password");
+                System.out.println("4. Exit");
+                System.out.println("Enter user input");
+
+                int usercase;
+                usercase= scanner.nextInt();
+                if(usercase == 1){
+                    loginUser();
+                }
+                else if(usercase ==2){
+                    studentRegistration();
+                }
+                else if(usercase == 3){
+                    updatePassword();
+                }
+                else{
+                    scanner.close();
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 
 	public void loginUser(){
-		System.out.println("\n\n----------User Log In---------\n\n");
-		Scanner userlogin = new Scanner(System.in);
-
-		String userId,password;
+        System.out.println("\n\n----------User Log In---------\n\n");
+		String useremail,password;
 		int role;
+        try{
+            System.out.println("Email:");
+            useremail = scanner.next();
+            System.out.println("Password:");
+            password = scanner.next();
+            System.out.println("Role:");
+            System.out.println("1.Admin		2.Professor		3.Student");
+            role = scanner.nextInt();
 
-		System.out.println("Email:");
-		userEmail = userlogin.next();
-		System.out.println("Password:");
-		password = userlogin.next();
-		System.out.println("Role:");
-		System.out.println("1.Admin		2.Professor		3.Student");
-		role = userlogin.nextInt();
+            while(role!=1 || role!=2 || role!=3)
+            {
+                System.out.println("***** Invalid Role please select correct role *****");
+                System.out.println("Role:");
+                System.out.println("1.Admin		2.Professor		3.Student");
+                role = scanner.nextInt();
+            }
 
-		while(role!=1 || role!=2 || role!=3)
-		{
-			system.out.println("***** Invalid Role please select correct role *****");
-			role = userlogin.nextInt();
-		}
+            if(verifyUser(useremail,password,role)){
+                loggedin = true;
+            }
+            else {
+                System.out.println("Invalid User");
+                loginUser();
+            }
 
-		if(verifyUser(userEmail,password,role)){
-			loggedin = true;
-		}
-		else {
-			System.out.println("Invalid User");
-			loginUser();
-		}
+            if (loggedin) {
+                switch (role) {
+                    case 1:
+                        System.out.println("\n\n----------Log In Successful as Admin---------\n\n");
+                        System.out.println("Welcome " + username + " !!");
+                        AdminCRSMenu adminMenu = new AdminCRSMenu();
+                        adminMenu.createAdminMenu(useremail);
+                        break;
+                    case 2:
+                        System.out.println("\n\n----------Log In Successful---------\n\n");
+                        ProfessorCRSMenu professorMenu=new ProfessorCRSMenu();
+                        professorMenu.createMenu();
 
-		if (loggedin) {
-			switch (role) {
-				case 1:
-					System.out.println("\n\n----------Log In Successful---------\n\n");
-					AdminCRSMenu adminMenu = new AdminCRSMenu();
-					adminMenu.createMenu();
-					break;
-				case 2:
-					System.out.println("\n\n----------Log In Successful---------\n\n");
-					ProfessorCRSMenu professorMenu=new ProfessorCRSMenu();
-					professorMenu.createMenu();
+                        break;
+                    case 3:
+                        System.out.println("\n\n----------Log In Successful---------\n\n");
+                        StudentCRSMenu studentMenu=new StudentCRSMenu();
+                        studentMenu.createMenu();
+                        break;
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
-					break;
-				case 3:
-					System.out.println("\n\n----------Log In Successful---------\n\n");
-					StudentCRSMenu studentMenu=new StudentCRSMenu();
-					studentMenu.createMenu();
-					break;
-			}
-		}
 	}
-    public static boolean verifyuser(String email, String pass, int role) throws IOException {
+    public static boolean verifyUser(String email, String pass, int role) throws IOException {
         File f = new File("../../../../resources/Sheet1.csv");
         FileReader fr = null;
         try {
