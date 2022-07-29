81b56f7656be8fe6416a11a585ec2b370d6a66fa
arpitadash <arpitadash1998@gmail.com>
2022-07-29 16:48:20 +0530

    Adding bean components similar to Shravani's github repo

--- CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/bean/Course.java
+++ CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/bean/Course.java
@@ -1,34 +1,60 @@
 package com.flipkart.bean;
 
+import java.util.ArrayList;
+
 public class Course {
     private String courseID;
     private String courseName;
     //	private String instructorId;
-    private int maxseats = 10;
-    private int minseats = 3;
+    private int seats = 10;
+    private String instructorId;
+
+    private ArrayList<Student> enrolledStudents;
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    /**
+     * @return the courseID
+     */
     public String getCourseID() {
         return courseID;
     }
+    /**
+     * @param courseID the courseID to set
+     */
     public void setCourseID(String courseID) {
         this.courseID = courseID;
     }
+    /**
+     * @return the courseName
+     */
     public String getCourseName() {
         return courseName;
     }
-
+    /**
+     * @param courseName the courseName to set
+     */
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
-
-//	public String getInstructorId() {
-//		return instructorId;
-//	}
-//	/**
-//	 * @param instructorId the instructorId to set
-//	 */
-//	public void setInstructorId(String instructorId) {
-//		this.instructorId = instructorId;
-//	}
+    /**
+     * @return the instructorId
+     */
+	public String getInstructorId() {
+		return instructorId;
+	}
+	/**
+	 * @param instructorId the instructorId to set
+	 */
+	public void setInstructorId(String instructorId) {
+		this.instructorId = instructorId;
+	}
     /**
      * @return the seats
      */
--- CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/bean/Professor.java
+++ CRS-JEDI-Grp5-Flipkart-POS/src/com/flipkart/bean/Professor.java
@@ -1,5 +1,32 @@
 package com.flipkart.bean;
 
 public class Professor {
+    private String professorID;
+    private String department;
 
+
+    /**
+     * @return the professorID
+     */
+    public String getProfessorID() {
+        return professorID;
+    }
+    /**
+     * @param professorID the professorID to set
+     */
+    public void setProfessorID(String professorID) {
+        this.professorID = professorID;
+    }
+    /**
+     * @return the department
+     */
+    public String getDepartment() {
+        return department;
+    }
+    /**
+     * @param department the department to set
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
